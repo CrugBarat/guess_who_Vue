b6c8bad16c2c0d1c4c69d2d16ce892bb2e7f4b29
Craig Barratt <craigtbarratt@gmail.com>
2020-04-28 13:12:28 +0100

    adds basic setup, fetches data from API and adds assets

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,40 @@
-<template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+<template lang="html">
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: 'app',
+  data() {
     return {
-      //
+      characters: [],
+      choices: []
     }
+  },
+  methods: {
+    buildUrl(path) {
+      const basUrl = "https://codeclan-guess-who.herokuapp.com/api/"
+      return basUrl + path
+    },
+
+    getCharacters(path) {
+      let url = this.buildUrl(path);
+      fetch(url).then(res => res.json())
+      .then(data => this.characters = data)
+    },
+
+    getChoices(path) {
+      let url = this.buildUrl(path);
+      fetch(url).then(res => res.json())
+      .then(data => this.choices = data)
+    }
+  },
+  mounted() {
+    this.getCharacters('characters');
+    this.getChoices('choices');
   }
 }
 </script>
+
+<style lang="css" scoped>
+</style>
